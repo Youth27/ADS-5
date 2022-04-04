a4561fb0df3b7cc0b47a9cfe5ab107802a5ab8db
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-04-04 19:58:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,8 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
-int znaki_pr(char znaki) {
-  switch (znaki) {
+int znaki_op(char znaki) {
+    switch (znaki) {
     case '(': return 0;
     case ')': return 1;
     case '+': return 2;
@@ -15,74 +14,73 @@ int znaki_pr(char znaki) {
     default: return 4;
     }
 }
-int operation(char znaki, int x, int y) {
+int calcul(char znaki, int x, int y) {
     switch (znaki) {
-      case '-': return (y - x);
-      case '+': return (y + x);
-      case '*': return (y * x);
-      case '/':
+    case '+': return (y + x);
+    case '-': return (y - x);
+    case '*': return (y * x);
+    case '/':
         if (x != 0)
-          return y / x;
-      default: return 0;
+            return y / x;
+    default: return 0;
     }
 }
 std::string infx2pstfx(std::string inf) {
-  std::string stroka_vivoda;
-  char space = ' ';
-  for (int i = 0; i < inf.size(); i++) {
-    if (znaki_pr(inf[i]) == 4) {
-      stroka_vivoda.push_back(inf[i]);
-      stroka_vivoda.push_back(space);
-    } else {
-      if (znaki_pr(inf[i]) == 0) {
-        stackk.push(inf[i]);
-      } else if (stackk.isEmpty()) {
-        stackk.push(inf[i]);
-      } else if ((znaki_pr(inf[i]) > znaki_pr(stackk.get()))) {
-        stackk.push(inf[i]);
-      } else if (znaki_pr(inf[i]) == 1) {
-        while (znaki_pr(stackk.get()) != 0) {
-          stroka_vivoda.push_back(stackk.get());
-          stroka_vivoda.push_back(space);
-          stackk.pop();
+  std::string output;
+    char space = ' ';
+    for (int i = 0; i < inf.size(); i++) {
+        if (znaki_op(inf[i]) == 4) {
+            output.push_back(inf[i]);
+            output.push_back(space);
+        } else {
+            if (znaki_op(inf[i]) == 0) {
+                stackk.push(inf[i]);
+            } else if (stackk.isEmpty()) {
+                stackk.push(inf[i]);
+            } else if ((znaki_op(inf[i]) > znaki_op(stackk.get()))) {
+                stackk.push(inf[i]);
+            } else if (znaki_op(inf[i]) == 1) {
+                while (znaki_op(stackk.get()) != 0) {
+                    output.push_back(stackk.get());
+                    output.push_back(probel);
+                    stackk.pop();
+                }
+                stackk.pop();
+            } else {
+                while (!stackk.isEmpty()
+                    && (znaki_op(inf[i]) <= priority(stackk.get()))) {
+                    output.push_back(stackk.get());
+                    output.push_back(probel);
+                    stackk.pop();
+                }
+                stackk.push(inf[i]);
+            }
         }
-        stackk.pop();
-      } else {
-        while (!stackk.isEmpty()
-               && (znaki_pr(inf[i]) <= znaki_pr(stackk.get()))) {
-          stroka_vivoda.push_back(stackk.get());
-          stroka_vivoda.push_back(space);
-          stackk.pop();
-        }
-        stackk.push(inf[i]);
-      }
     }
-  }
-while (!stackk.isEmpty()) {
-  stroka_vivoda.push_back(stack1.get());
-  stroka_vivoda.push_back(probel);
-  stackk.pop();
+    while (!stackk.isEmpty()) {
+        output.push_back(stackk.get());
+        output.push_back(space);
+        stackk.pop();
+    }
+    for (int i = 0; i < outp.size(); i++) {
+        if (output[output.size() - 1] == ' ')
+            output.erase(output.size() - 1);
+    }
+    return output;
 }
-for (int j = 0; j < stroka_vivoda.size(); j++) {
-  if (stroka_vivoda[stroka_vivoda.size() - 1] == ' ')
-    stroka_vivoda.erase(stroka_vivoda.size() - 1);
-}
-return stroka_vivoda;
-}
-
 int eval(std::string pref) {
   int result = 0;
-  for (int k = 0; i < pref.size(); k++) {
-    if (priority(pref[k]) == 4) {
-      stacckk.push(pref[k] - '0');
-    } else if (znaki_pr(pref[k]) < 4) {
-      int a = stacckk.get();
-      stacckk.pop();
-      int b = stacckk.get();
-      stacckk.pop();
-      stacckk.push(operation(pref[k], x, y));
+    for (int i = 0; i < pref.size(); i++) {
+        if (znaki_op(pref[i]) == 4) {
+            stacckk.push(pref[i] - '0');
+        } else if (znaki_op(pref[i]) < 4) {
+            int x = stacckk.get();
+            stackckk.pop();
+            int y = stacckk.get();
+            stacckk.pop();
+            stacckk.push(calcul(pref[i], x, y));
+        }
     }
-  }
-  result = stacckk.get();
-  return result;
+    result = stacckk.get();
+    return result;
 }
