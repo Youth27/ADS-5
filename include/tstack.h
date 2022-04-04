4db7791f5890ba9dd208840ba3c6cf85792481a8
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-04-04 20:31:24 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,13 +1,15 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+
 template<typename T, int size>
 class TStack {
 private:
 T arr[100];
 int top;
+
 public:
-TStack(): top(-1) { }
+TStack() :top(-1) { }
 bool isEmpty() {
 return top == -1;
 }
