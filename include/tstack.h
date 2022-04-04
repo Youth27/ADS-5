eb15b2fffe575ad088c930e31d5595995dc0fe5e
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-04-04 20:45:54 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -10,9 +10,6 @@ int top;
 
  public:
 TStack() :top(-1) { }
-bool isEmpty() {
-return top == -1;
-}
 T get() {
 if (!isEmpty())
 return arr[top];
@@ -21,7 +18,7 @@ throw "Empty!";
 }
 void pop() {
 if (!isEmpty())
---top;
+top--;
 }
 void push(T value) {
 if (isFull())
