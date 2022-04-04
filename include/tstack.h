3b9bf37a680ad8273040c712c356175869578147
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-04-04 03:30:30 +0300

    Артём Маричев

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,37 @@
 
 template<typename T, int size>
 class TStack {
-
+private:
+  T arr[100];
+  int top; 
+public:
+  TStack(): top(-1) { }
+  bool isEmpty() {
+  return top == -1;
+  }
+  T get() {
+      if (!isEmpty())
+          return arr[top];
+      else
+          throw "Empty!";
+  }
+  void pop() {
+      if (!isEmpty())
+          --top;
+  }
+  void push(T value) {
+      if (isFull())
+          arr[++top] = value;
+      else
+          throw "Full!";
+  }
+  bool isEmpty() const {
+      return top == -1;
+  }
+  bool isFull() const {
+      return top == size - 1;
+  }
 };
 #endif  // INCLUDE_TSTACK_H_
+TStack<char, 100> stackk;
+TStack<int, 100> stacckk;
