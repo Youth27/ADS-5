2a2493eb79ba5058e5421a5ee8347ab4fbd51437
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-04-04 03:34:20 +0300

    Артём Маричев

--- include/tstack.h
+++ include/tstack.h
@@ -5,35 +5,35 @@
 template<typename T, int size>
 class TStack {
 private:
-  T arr[100];
-  int top; 
+T arr[100];
+int top; 
 public:
-  TStack(): top(-1) { }
-  bool isEmpty() {
-  return top == -1;
-  }
-  T get() {
-      if (!isEmpty())
-          return arr[top];
-      else
-          throw "Empty!";
-  }
-  void pop() {
-      if (!isEmpty())
-          --top;
-  }
-  void push(T value) {
-      if (isFull())
-          arr[++top] = value;
-      else
-          throw "Full!";
-  }
-  bool isEmpty() const {
-      return top == -1;
-  }
-  bool isFull() const {
-      return top == size - 1;
-  }
+TStack(): top(-1) { }
+bool isEmpty() {
+return top == -1;
+}
+T get() {
+if (!isEmpty())
+return arr[top];
+else
+throw "Empty!";
+}
+void pop() {
+if (!isEmpty())
+--top;
+}
+void push(T value) {
+if (isFull())
+arr[++top] = value;
+else
+throw "Full!";
+}
+bool isEmpty() const {
+return top == -1;
+}
+bool isFull() const {
+return top == size - 1;
+}
 };
 #endif  // INCLUDE_TSTACK_H_
 TStack<char, 100> stackk;
